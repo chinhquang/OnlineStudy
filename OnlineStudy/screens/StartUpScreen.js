eacd5f2ef4e2a69a86d02f7835ca4f615e604b52
chinhquang <trinhquangchinh171298@gmail.com>
2020-05-13 21:22:41 +0700

    commit

--- OnlineStudy/screens/StartUpScreen.js
+++ OnlineStudy/screens/StartUpScreen.js
@@ -13,9 +13,12 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
+  StatusBar, 
+  Dimensions,
+  Image
+  
 } from 'react-native';
-
+import LinearGradient from 'react-native-linear-gradient'
 import {
   Header,
   LearnMoreLinks,
@@ -23,54 +26,24 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+const {width, height} = Dimensions.get('window');
 
 export default class  StartUpScreen extends Component{
+    
     render (){
         return (
             <>
         <StatusBar barStyle="light-content" />
-        <SafeAreaView>
-        <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}>
-            <Header />
-            {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-                <Text style={styles.footer}>Engine: Hermes</Text>
+        
+        <LinearGradient colors={['rgba(38, 50, 56, 1)', 'rgba(38, 50, 56, 0.7)']} style = { styles.container }>
+
+            <View style={styles.iconContainer}>
+                <Image style={styles.icon} source={require('../image/icon.png')}/>
+                <Text style={styles.iconTitle}>polygon runway</Text>
             </View>
-            )}
-            <View style={styles.body}>
-                <View style={styles.sectionContainer}>
-                    <Text style={styles.sectionTitle}>Step One</Text>
-                    <Text style={styles.sectionDescription}>
-                    Edit 
-                    <Text style={styles.highlight}>App.js</Text>
-                    to change this
-                    screen and then come back to see your edits.
-                    </Text>
-                </View>
-                <View style={styles.sectionContainer}>
-                    <Text style={styles.sectionTitle}>See Your Changes</Text>
-                    <Text style={styles.sectionDescription}>
-                    <ReloadInstructions />
-                    </Text>
-                </View>
-                <View style={styles.sectionContainer}>
-                    <Text style={styles.sectionTitle}>Debug</Text>
-                    <Text style={styles.sectionDescription}>
-                    <DebugInstructions />
-                    </Text>
-                </View>
-                <View style={styles.sectionContainer}>
-                    <Text style={styles.sectionTitle}>Learn More</Text>
-                    <Text style={styles.sectionDescription}>
-                    Read the docs to discover what to do next:
-                    </Text>
-                </View>
-                <LearnMoreLinks />
-            </View>
-        </ScrollView>
-        </SafeAreaView>
+            
+        </LinearGradient>
+        
         </>
         );
     }
@@ -78,41 +51,43 @@ export default class  StartUpScreen extends Component{
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container : {
+        width : '100%',
+        height : '100%'
+       
+    },
+    iconContainer : {
+        // display: 'flex',
+        // position : 'absolute',
+        // flexDirection : 'row',
+        top : 0.26 * width,
+        alignSelf : 'center',
+        width: 205/375 * width,
+        aspectRatio: 205 / 125,
+        // backgroundColor : '#000000'
+    },
+    icon :{
+        flex:1 ,
+        width: 100/375 * width,
+        aspectRatio : 1,
+        resizeMode : 'contain',
+        alignSelf : 'center',
+        top : 0,
+    },
+    iconTitle: {
+        alignSelf : 'center',
+        color : '#ffffff',
+        fontFamily: 'Copperplate',
+        width : '100%',
+        bottom : 0,
+        top : 0,
+        textAlign : 'center',
+        fontWeight: 'normal',
+        fontStyle :'normal',
+        lineHeight: 25,
+        // backgroundColor : '#FAABBB',
+        fontSize: 24 * width/ 375
+
+    }
 });
 
